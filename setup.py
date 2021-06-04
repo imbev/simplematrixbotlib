6640e3256c190a32ce88402e4a82719afd45d1bc
krazykirby99999 <krazykirby99999@gmail.com>
2021-06-04 17:13:23 -0500

    Increase version numbers

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-VERSION = '1.0.0' 
+VERSION = '1.0.1' 
 DESCRIPTION = 'An easy to use bot library for the Matrix ecosystem written in Python.'
 with open(path.join(os.path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
     LONG_DESCRIPTION = f.read()
