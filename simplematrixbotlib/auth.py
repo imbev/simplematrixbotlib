299bee3b4f12cf19f5f7da87329497b647f0cbf7
krazykirby99999 <krazykirby99999@gmail.com>
2021-06-10 17:15:28 -0500

    Write numpy style docstrings for the Creds class

--- simplematrixbotlib/auth.py
+++ simplematrixbotlib/auth.py
@@ -1,8 +1,38 @@
 class Creds:
     """
-    A class to store and handle login credentials
+    A class to store and handle login credentials.
+
+    ...
+
+    Attributes
+    ----------
+    homeserver : str
+        The homeserver for the bot to connect to. Begins with "https://".
+    
+    username : str
+        The username for the bot to connect as.
+    
+    password : str
+        The password for the bot to connect with.
+
     """
     def __init__(self, homeserver, username, password):
+        """
+        Initializes the simplematrixbotlib.Creds class.
+
+        Parameters
+        ----------
+        homeserver : str
+            The homeserver for the bot to connect to. Begins with "https://".
+    
+        username : str
+            The username for the bot to connect as.
+    
+        password : str
+            The password for the bot to connect with.
+
+        """
+
         self.homeserver = homeserver
         self.username = username
         self.password = password
