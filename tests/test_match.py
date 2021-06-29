62876c251bca614905bdb0109c33091c6a0206fc
krazykirby99999 <krazykirby99999@gmail.com>
2021-06-29 10:37:20 -0500

    Add tests for MessageMatch class

---
+++ tests/test_match.py
@@ -0,0 +1,60 @@
+from path_fix import *
+import unittest
+from unittest import mock
+import simplematrixbotlib as botlib
+
+class IntegrationMatchTest(unittest.TestCase):
+    def setUp(self):
+        self.mock_room = mock.Mock()
+        self.mock_message = mock.Mock()
+        self.mock_bot = mock.Mock()
+    
+    def test_single_word_message_body(self):
+        self.mock_message.body = 'hello'
+
+        message_match = botlib.MessageMatch(self.mock_room, self.mock_message, self.mock_bot)
+        self.assertTrue(message_match.prefix('hello'))
+
+        message_match = botlib.MessageMatch(self.mock_room, self.mock_message, self.mock_bot)
+        self.assertTrue(message_match.command('hello'))
+
+        message_match = botlib.MessageMatch(self.mock_room, self.mock_message, self.mock_bot)
+        self.assertFalse(message_match.command('ello'))
+        self.assertTrue(message_match.prefix('h'))
+        self.assertTrue(message_match.command('ello'))
+
+        message_match = botlib.MessageMatch(self.mock_room, self.mock_message, self.mock_bot)
+        message_match.command('hello')
+        self.assertFalse(message_match.args)
+
+        message_match = botlib.MessageMatch(self.mock_room, self.mock_message, self.mock_bot)
+        self.assertTrue(message_match.contains('hello'))
+        self.assertTrue(message_match.contains('h'))
+        self.assertTrue(message_match.contains('lo'))
+        self.assertFalse(message_match.contains('he llo'))
+    
+    def test_prefix_command_with_args_message_body_different_user_ids(self):
+        self.mock_message.body = '!mycommand arg1 arg2'
+        self.mock_message.sender = '1234'
+        self.mock_bot.async_client.user_id = '5678'
+        message_match = botlib.MessageMatch(self.mock_room, self.mock_message, self.mock_bot)
+
+        self.assertTrue(
+            message_match.prefix('!') and message_match.command('mycommand') and message_match.not_from_this_bot()
+            )
+        
+        self.assertEqual(message_match.args[0], 'arg1')
+        self.assertEqual(message_match.args[1], 'arg2')
+
+        self.assertTrue(message_match.contains('!'))
+        self.assertTrue(message_match.contains('!mycommand'))
+        self.assertTrue(message_match.contains('!mycommand arg1 arg2'))
+        self.assertTrue(message_match.contains('mycommand'))
+        self.assertTrue(message_match.contains('arg1 arg2'))
+        self.assertTrue(message_match.contains('a'))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+        
