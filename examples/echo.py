14f80ad2d3aa5a91cc5918287584182999d805b6
krazykirby99999 <krazykirby99999@gmail.com>
2021-08-24 00:27:34 +0000

    Update echo example

--- examples/echo.py
+++ examples/echo.py
@@ -9,19 +9,19 @@ echo_bot
 """
 
 import simplematrixbotlib as botlib
-import os
 
-creds = botlib.Creds("https://example.org", "echo_bot", "secretpassword")
+creds = botlib.Creds("https://home.server", "user", "pass")
 bot = botlib.Bot(creds)
-
-prefix = '!'
+PREFIX = '!'
 
 @bot.listener.on_message_event
 async def echo(room, message):
-    match = botlib.MessageMatch(room, message, bot)
-    if match.not_from_this_bot() and match.prefix(prefix) and match.command(
-            "echo"):
-        await bot.api.send_text_message(room.room_id, match.args)
+    match = botlib.MessageMatch(room, message, bot, PREFIX)
 
+    if match.is_not_from_this_bot() and match.prefix() and match.command("echo"):
+
+        await bot.api.send_text_message(
+            room.room_id, " ".join(arg for arg in match.args())
+            )
 
 bot.run()
