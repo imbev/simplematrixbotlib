4e4bd1bf8772ce51a32f2dd8e17d8b38f4ee00c2
krazykirby99999 <krazykirby99999@gmail.com>
2021-09-06 14:39:17 -0500

    Add match is_from_userid test

--- tests/match/test_match.py
+++ tests/match/test_match.py
@@ -1,9 +1,17 @@
 from simplematrixbotlib.match import Match
 from unittest import mock
 
-match = Match(mock.MagicMock(), mock.MagicMock(), mock.MagicMock())
+mock_room = mock.MagicMock()
+mock_event = mock.MagicMock()
+mock_bot = mock.MagicMock()
+match = Match(mock_room, mock_event, mock_bot)
 
 def test_init():
     assert isinstance(match.room, mock.MagicMock)
     assert isinstance(match.event, mock.MagicMock)
     assert isinstance(match._bot, mock.MagicMock)
+
+def test_is_from_userid():
+    mock_event.sender = "123"
+    assert match.is_from_userid("123")
+    assert not match.is_from_userid("456")
