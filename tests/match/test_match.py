278e78490e5fd2797aa239d56982f9d3fd8fa5cf
krazykirby99999 <krazykirby99999@gmail.com>
2021-09-06 14:41:59 -0500

    Add is_not_from_this_bot test

--- tests/match/test_match.py
+++ tests/match/test_match.py
@@ -2,8 +2,13 @@ from simplematrixbotlib.match import Match
 from unittest import mock
 
 mock_room = mock.MagicMock()
+
 mock_event = mock.MagicMock()
+mock_event.sender = "123"
+
 mock_bot = mock.MagicMock()
+mock_bot.async_client.user_id = "botid"
+
 match = Match(mock_room, mock_event, mock_bot)
 
 def test_init():
@@ -12,6 +17,8 @@ def test_init():
     assert isinstance(match._bot, mock.MagicMock)
 
 def test_is_from_userid():
-    mock_event.sender = "123"
     assert match.is_from_userid("123")
     assert not match.is_from_userid("456")
+
+def test_is_not_from_this_bot():
+    assert match.is_not_from_this_bot()
