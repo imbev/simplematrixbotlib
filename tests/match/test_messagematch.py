84136544729b4b19d291320f0092abd9d049fbef
krazykirby99999 <krazykirby99999@gmail.com>
2021-09-06 16:01:54 -0500

    Add args test

--- tests/match/test_messagematch.py
+++ tests/match/test_messagematch.py
@@ -6,6 +6,9 @@ mock_room = mock.MagicMock()
 mock_event = mock.MagicMock()
 mock_event.body = "p!help example"
 
+mock_event2 = mock.MagicMock()
+mock_event2.body = "p!help"
+
 mock_bot = mock.MagicMock()
 
 prefix = "p!"
@@ -14,6 +17,7 @@ prefix2 = "!!"
 match = MessageMatch(mock_room, mock_event, mock_bot, prefix)
 match2 = MessageMatch(mock_room, mock_event, mock_bot)
 match3 = MessageMatch(mock_room, mock_event, mock_bot, prefix2)
+match4 = MessageMatch(mock_room, mock_event2, mock_bot, prefix)
 
 def test_init():
     assert issubclass(MessageMatch, Match)
@@ -31,3 +35,8 @@ def test_prefix():
     assert match3.prefix() == False
 
     #assert match2.prefix() == True
+
+def test_args():
+    assert match.args() == ["example"]
+
+    assert match4.args() == []
