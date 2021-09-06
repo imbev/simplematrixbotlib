23713748d30bec61cfde9925400968aaaf4da747
krazykirby99999 <krazykirby99999@gmail.com>
2021-09-06 15:50:09 -0500

    Add command test

--- tests/match/test_messagematch.py
+++ tests/match/test_messagematch.py
@@ -4,13 +4,22 @@ from unittest import mock
 mock_room = mock.MagicMock()
 
 mock_event = mock.MagicMock()
+mock_event.body = "p!help example"
 
 mock_bot = mock.MagicMock()
 
 prefix = "p!"
 
 match = MessageMatch(mock_room, mock_event, mock_bot, prefix)
+match2 = MessageMatch(mock_room, mock_event, mock_bot)
 
 def test_init():
     assert issubclass(MessageMatch, Match)
     assert match._prefix == prefix
+
+def test_command():
+    assert match.command() == "help"
+    assert match.command("help") == True
+
+    assert match2.command() == "p!help"
+    assert match2.command("p!help") == True
