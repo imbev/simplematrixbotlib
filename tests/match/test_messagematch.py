f982abec9c3e6451412546adc82fbd609d7cad61
KrazyKirby99999 <69373044+KrazyKirby99999@users.noreply.github.com>
2021-09-06 16:05:59 -0500

    Add tests for match.py (#64)
    * Add pytest to dev dependencies
    
    * Add sample test
    
    * Update make test command
    
    * Remove sample test
    
    * Add match init test
    
    * Add match is_from_userid test
    
    * Add is_not_from_this_bot test
    
    * Add messagematch init test
    
    * Add command test
    
    * Add prefix test. IMPORTANT MessageMatch.prefix is implemented incorrectly! "If there is no prefix specified during the creation of this MessageMatch object, then return True." Returns False instead. Part of the test is currently disabled, and will need to be re-enabled later
    
    * Add args test
    
    * Add contains test

--- tests/match/test_messagematch.py
+++ tests/match/test_messagematch.py
@@ -4,10 +4,48 @@ from unittest import mock
 mock_room = mock.MagicMock()
 
 mock_event = mock.MagicMock()
+mock_event.body = "p!help example"
+
+mock_event2 = mock.MagicMock()
+mock_event2.body = "p!help"
 
 mock_bot = mock.MagicMock()
 
 prefix = "p!"
+prefix2 = "!!"
+
+match = MessageMatch(mock_room, mock_event, mock_bot, prefix)
+match2 = MessageMatch(mock_room, mock_event, mock_bot)
+match3 = MessageMatch(mock_room, mock_event, mock_bot, prefix2)
+match4 = MessageMatch(mock_room, mock_event2, mock_bot, prefix)
+
+def test_init():
+    assert issubclass(MessageMatch, Match)
+    assert match._prefix == prefix
+
+def test_command():
+    assert match.command() == "help"
+    assert match.command("help") == True
+
+    assert match2.command() == "p!help"
+    assert match2.command("p!help") == True
+
+def test_prefix():
+    assert match.prefix() == True
+    assert match3.prefix() == False
+
+    #assert match2.prefix() == True
+
+def test_args():
+    assert match.args() == ["example"]
+
+    assert match4.args() == []
+
+def test_contains():
+    assert match.contains("!h") == True
+    assert match.contains("lp exam") == True
+    assert match.contains("nothing") == False
+=======
 
 match = MessageMatch(mock_room, mock_event, mock_bot, prefix)
 
