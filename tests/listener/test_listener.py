27a1a6d7785d9b5d7999b8b7cf34c343385150b8
krazykirby99999 <krazykirby99999@gmail.com>
2021-09-06 16:44:45 -0500

    Add on_message_event test

--- tests/listener/test_listener.py
+++ tests/listener/test_listener.py
@@ -1,6 +1,7 @@
 from typing import List
 from simplematrixbotlib.listener import Listener
 from unittest import mock
+from nio import RoomMessageText
 
 mock_bot = mock.MagicMock()
 
@@ -21,6 +22,19 @@ def test_on_custom_event():
     def check():
         for func_event in listener._registry:
             if func_event[0]() == "example" and func_event[1] == mock_event:
+                return True
+        return False
+    
+    assert check() == True
+
+def test_on_message_event():
+    @listener.on_message_event
+    def example2():
+        return "example2"
+    
+    def check():
+        for func_event in listener._registry:
+            if func_event[0]() == "example2" and func_event[1] == RoomMessageText:
                 return True
         return False
     
