35f4bb040983031b67469f06ae3ceda5a28f1039
HarHarLinks <kim.brose@rwth-aachen.de>
2021-12-01 16:37:34 +0100

    refactor: :recycle: use os.path.join

--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -1,8 +1,8 @@
 import pathlib
 import simplematrixbotlib as botlib
+import os.path
 
-
-sample_config_path = f'{pathlib.Path(__file__).parent}/sample_config_files'
+sample_config_path = os.path.join(pathlib.Path(__file__).parent, 'sample_config_files')
 
 def test_defaults():
     config = botlib.Config()
@@ -13,18 +13,18 @@ def test_defaults():
 
 def test_read_toml():
     config = botlib.Config()
-    config.load_toml(f'{sample_config_path}/config1.toml')
     assert not config._join_on_invite
+    config.load_toml(os.path.join(sample_config_path, 'config1.toml'))
     assert config.allowlist == ['*:example.org', '@test:matrix.org']
     assert config.blocklist == ['@test2:example.org']
         
     config = botlib.Config()
-    config.load_toml(f'{sample_config_path}/config2.toml')
     assert config._join_on_invite
+    config.load_toml(os.path.join(sample_config_path, 'config2.toml'))
 
     config = botlib.Config()
-    config.load_toml(f'{sample_config_path}/config3.toml')
     assert config._join_on_invite
+    config.load_toml(os.path.join(sample_config_path, 'config3.toml'))
 
 def test_manual_set():
     config = botlib.Config()
