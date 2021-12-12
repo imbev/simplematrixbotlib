10cedaa489cd4e6d196b28100bd2fa2bbd10a888
HarHarLinks <kim.brose@rwth-aachen.de>
2021-12-12 19:03:44 +0100

    test: :white_check_mark: Config `@property` and validation

--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -10,8 +10,8 @@ def test_defaults():
     config = botlib.Config()
 
     assert config.join_on_invite
-    assert config.allowlist == list()
-    assert config.blocklist == list()
+    assert config.allowlist == set()
+    assert config.blocklist == set()
 
 def test_read_toml():
     config = botlib.Config()
@@ -30,15 +30,34 @@ def test_read_toml():
 
     config = botlib.Config()
     # config4.toml uses invalid regular expression syntax
-    with pytest.raises(re.error):
-        assert config.load_toml(os.path.join(sample_config_path, 'config4.toml'))
+    config.load_toml(os.path.join(sample_config_path, 'config4.toml'))
+    assert config.blocklist == set()
 
     # TODO: test botlib.Bot() constructor creating a default Config
 
 def test_manual_set():
     config = botlib.Config()
-    config.set_join_on_invite(True)
+    config.join_on_invite = True
     assert config.join_on_invite
 
-    config.set_join_on_invite(False)
+    config.join_on_invite = False
     assert not config.join_on_invite
+
+    config = botlib.Config()
+    config.allowlist = {'.*:example\\.org'}
+    assert re.compile('.*:example\\.org') in config.allowlist
+    config.add_allowlist({'@test:matrix\\.org'})
+    assert config.allowlist == set(map(re.compile, ['.*:example\\.org', '@test:matrix\\.org']))
+    config.remove_allowlist({'.*:example\\.org'})
+    assert config.allowlist == set(map(re.compile, ['@test:matrix\\.org']))
+    config.allowlist = {'*:example\\.org'}
+    assert config.allowlist == set(map(re.compile, ['@test:matrix\\.org']))
+
+    config.blocklist = {'.*:example\\.org'}
+    assert re.compile('.*:example\\.org') in config.blocklist
+    config.add_blocklist({'@test:matrix\\.org'})
+    assert config.blocklist == set(map(re.compile, ['.*:example\\.org', '@test:matrix\\.org']))
+    config.remove_blocklist({'.*:example\\.org'})
+    assert config.blocklist == set(map(re.compile, ['@test:matrix\\.org']))
+    config.blocklist = {'*:example\\.org'}
+    assert config.blocklist == set(map(re.compile, ['@test:matrix\\.org']))
