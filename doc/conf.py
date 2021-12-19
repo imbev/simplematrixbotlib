c0e39efaba52832301cc250764d51435a6a4e2fc
KrazyKirby99999 <krazykirby99999@gmail.com>
2021-12-19 18:09:10 +0000

    chore: :bookmark: Bump version to 2.5.1
    Change version number to 2.5.1 in poetry config, sphinx config, and readme.

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, KrazyKirby99999'
 author = 'KrazyKirby99999'
 
 # The full version, including alpha/beta/rc tags
-release = '2.5.0'
+release = '2.5.1'
 
 # -- General configuration ---------------------------------------------------
 
