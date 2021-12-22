9b7810a0881e6b6f3aa80876dda9ce11245dc358
krazykirby99999 <krazykirby99999@gmail.com>
2021-12-21 20:04:20 -0600

    test: :white_check_mark: Add tests for Api.check_valid_homeserver

---
+++ tests/api/test_api.py
@@ -0,0 +1,26 @@
+import pytest
+import simplematrixbotlib as botlib
+
+def test_check_valid_homeserver():
+
+    creds = botlib.Creds("https://example.com", "user", "pass")
+    bot = botlib.Bot(creds)
+    with pytest.raises(ValueError):
+        bot.run()
+
+    creds = botlib.Creds("matrix.org", "user", "pass")
+    bot = botlib.Bot(creds)
+    with pytest.raises(ValueError):
+        bot.run()
+
+    creds = botlib.Creds("https://matrix.org", "user", "pass")
+    bot = botlib.Bot(creds)
+    try:
+        bot.run()
+    except Exception as e:
+        #Trying to catch LoginError normally causes an error - TypeError: catching classes that do not inherit from BaseException is not allowed
+        #This is an issue with matrix-nio
+        if 'LoginError: M_FORBIDDEN Invalid password' in str(e):
+            pass
+        else:
+            raise e
