b1a03a3ff79777a63fb2ed65115644179907eff6
Isaac Beverly <imbev@protonmail.com>
2022-10-13 16:37:41 +0000

    doc: Update docs config for v2.7.2

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, KrazyKirby99999'
 author = 'KrazyKirby99999'
 
 # The full version, including alpha/beta/rc tags
-release = '2.7.0'
+release = '2.7.2'
 
 # -- General configuration ---------------------------------------------------
 
